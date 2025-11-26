02c942c079a8a5fa42e83aceb319bb17c6d1d180
Ramesh Dura <kals.future@gmail.com>
2025-11-26 22:03:30 +0900

    Update echo statement in first.php

--- first.php
+++ first.php
@@ -1,4 +1,4 @@
 <?php
 echo "this is the first file   ";
-echo "  edited -----  TEST ---/DKFD99KJF// I UPDATED";
+echo "  edited -----  TEST ---/DKFD99KJF// I UPDATED nice folder this is a nice folder";
 ?>
